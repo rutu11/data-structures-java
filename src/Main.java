8905bc3c394df6ff5514f6be79e4a74d5afbfa96
Rutuja Pujare <rutujapoojare@gmail.com>
2025-03-08 23:13:15 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int[] arr = {3, 8, 9, 2};
+
+//        SortingAlogs.selectionSort(arr);
+//        SortingAlogs.bubbleSort(arr);
+        SortingAlogs.insertionSort(arr);
+
+    }
+
+
+}
